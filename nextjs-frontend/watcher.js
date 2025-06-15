2767ba5494ea2dc1aced2968403933906e0804d9
theo-michel <theo64200@gmail.com>
2025-06-15 13:13:36 +0200

    watcher changes

--- nextjs-frontend/watcher.js
+++ nextjs-frontend/watcher.js
@@ -6,6 +6,12 @@ config({ path: ".env.local" });
 
 const openapiFile = process.env.OPENAPI_OUTPUT_FILE;
 
+// Check if the environment variable is defined
+if (!openapiFile) {
+  console.error("Error: OPENAPI_OUTPUT_FILE environment variable is not defined in .env.local");
+  process.exit(1);
+}
+
 // Watch the specific file for changes
 chokidar.watch(openapiFile).on("change", (path) => {
   console.log(`File ${path} has been modified. Running generate-client...`);
